b0dce2743f6a852a40209c58abc6f3b6d1aae891
mizunara0126 <141396090+mizunara0126@users.noreply.github.com>
2023-08-04 16:43:43 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,27 @@
+$(function() {
+  $(".slide-items").slick({
+    autoplay:true,
+    slidesToShow:3,
+    infinite:true,
+    slidesToScroll:1,
+    arrows:false,
+    dots:true,
 
+    responsive: [{
+      breakpoint: 960,
+       settings: {
+        slidesToShow: 1,
+        centerMode: true,
+        centerPadding: '20%',
+      },
+     },
+    ]
+  });
+});
+
+const ham = $('#js-hamburger');
+const nav = $('#js-nav');
+ham.on('click', function () { //ハンバーガーメニューをクリックしたら
+ham.toggleClass('active'); // ハンバーガーメニューにactiveクラスを付け外し
+nav.toggleClass('active'); // ナビゲーションメニューにactiveクラスを付け外
+});
